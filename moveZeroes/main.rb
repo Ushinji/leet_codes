0b031cfd2336d57e9d2b4938f977a9cacf7b5d94
Ushinji <siguma971@gmail.com>
2020-12-23 18:26:31 +0900

    Update move_zeroes

--- moveZeroes/main.rb
+++ moveZeroes/main.rb
@@ -1,20 +1,14 @@
-def find_non_zero(nums, start)
-  (start...nums.size).each do |i|
-    return i if nums[i] != 0
-  end
-  nil
-end
-
 # @param {Integer[]} nums
 # @return {Void} Do not return anything, modify nums in-place instead.
 def move_zeroes(nums)
   left = 0
-  while true
-    right = find_non_zero(nums, left)
-    break if right.nil?
-
-    nums[right], nums[left] = nums[left], nums[right]
-    left += 1
+  right = 0
+  while right < nums.length
+    if nums[right] != 0
+      nums[right], nums[left] = nums[left], nums[right]
+      left += 1
+    end
+    right += 1
   end
   nums
 end
