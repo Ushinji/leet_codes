ba22aad4ccd325e63a7644d5ac114eaa4f6e1d25
Ushinji <siguma971@gmail.com>
2021-02-27 10:24:34 +0900

    [Add]Add solution to 374_guess_number_higher_or_lower

---
+++ 374_guess_number_higher_or_lower/main.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if my number is lower, 1 if my number is higher, otherwise return 0
+# def guess(num)
+
+def guessNumber(n)
+  left = 1
+  right = n
+
+  while left < right
+    mid = left + (right - left) / 2
+    res = guess(mid)
+    case res
+    when -1
+      right = mid - 1
+    when 1
+      left = mid + 1
+    else
+      return mid
+    end
+  end
+  right
+end
